dcf9b94c99e0875faada5491421d3563be2a7e20
Duksing <chauduksing2@gmail.com>
2024-03-21 18:55:08 -0400

    1. added checker so that you can't stack the same classes

--- frontEnd/src/components/ClassForm.js
+++ frontEnd/src/components/ClassForm.js
@@ -35,15 +35,23 @@ function ClassForm() {
  };
 
  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (selectedClass === '') {
-      setErrorMessage('Please select a class before submitting.');
-      return;
-    }
-    setSelectedClasses([...selectedClasses, { id: selectedClasses.length, name: selectedClass }]);
-    setSelectedClass('');
-    setErrorMessage(''); // Clear the error message after successful submission
+  event.preventDefault();
+  if (selectedClass === '') {
+     setErrorMessage('Please select a class before submitting.');
+     return;
+  }
+  // Check if the selected class is already in the selectedClasses array
+  const classExists = selectedClasses.some(classItem => classItem.name === selectedClass);
+  if (classExists) {
+     setErrorMessage('This class has already been added.');
+     return;
+  }
+  // If the class does not exist, add it to the selectedClasses array
+  setSelectedClasses([...selectedClasses, { id: selectedClasses.length, name: selectedClass }]);
+  setSelectedClass('');
+  setErrorMessage(''); // Clear the error message after successful submission
  };
+ 
 
  const handleRemoveClass = (id) => {
     setSelectedClasses(selectedClasses.filter(classItem => classItem.id !== id));
